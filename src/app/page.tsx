3860dafb2baef04ccbf2b9dd16029a0b5214387c
codingpalette <codingpalette@gmail.com>
2024-01-12 17:38:50 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <Button>ssss</Button>
+      </div>
+    </main>
+  )
+}
