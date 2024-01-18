e2159c671e1eb27f8963707498150fe634ecf491
codingpalette <codingpalette@gmail.com>
2024-01-18 15:40:33 +0900

    supabase 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,11 @@
-"use client";
+import supabase from "@/utils/supabase";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-export default function Home() {
-  const { setTheme } = useTheme();
+export default async function Home() {
+  const { data } = await supabase.from("page").select("*");
+  console.log("data", data);
 
   return (
     <>
-      {/* <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>
-            Deploy your new project in one-click.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>dsfsdf</CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card> */}
-
       <div>2222</div>
     </>
   );
